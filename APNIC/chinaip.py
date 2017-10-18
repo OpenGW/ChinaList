667d1a154cd6be942e3ceb3defef68035525d5c0
OpenGW <opengw@fastmail.com>
2017-10-18 21:08:24 +0800

    Remove delegated-apnic-latest
    - delegated-apnic-latest is subset of delegated-apnic-extended-latest
    - modify chinaip.py accordingly
    - rename APNIC/APNIC.md to APNIC/README.md

--- APNIC/chinaip.py
+++ APNIC/chinaip.py
@@ -16,17 +16,16 @@ def main(args):
     # type: (list[str]) -> None
     """
     Command:
-        chinaip.py <apnic_file_path> <countries> <rectypes>
+        chinaip.py <countries> <record_types>
     Example:
-        chinaip.py <apnic_file_path> CN ipv4
-        chinaip.py <apnic_file_path> CN,HK,TW ipv4,ipv6
+        chinaip.py CN ipv4
+        chinaip.py CN,HK,TW ipv4,ipv6
     """
-    assert len(args) == 3
-    apnic_file_path = args[0]
-    countries = set(map(str.upper, filter(lambda s: s, args[1].split(","))))
-    rectypes = set(map(str.upper, filter(lambda s: s, args[2].split(","))))
+    assert len(args) == 2
+    countries = set(map(str.upper, filter(lambda s: s, args[0].split(","))))
+    record_types = set(map(str.upper, filter(lambda s: s, args[1].split(","))))
 
-    with codecs.open(apnic_file_path, "r", "utf-8") as fp:
+    with codecs.open("delegated-apnic-extended-latest", "r", "utf-8") as fp:
         lines = fp.readlines()
 
     for line in lines:
@@ -34,29 +33,27 @@ def main(args):
         if line.startswith("#"):
             continue
 
-        # delegated-apnic-latest example:
-        #   apnic|AU|ipv4|1.0.0.0|256|20110811|assigned
-        # delegated-apnic-extended-latest example:
+        # Example:
         #   apnic|AU|ipv4|1.0.0.0|256|20110811|assigned|A91872ED
         # Parts:
-        #       0| 1|   2|      3|  4|       5|       6|
+        #       0| 1|   2|      3|  4|       5|       6|       7
         parts = line.split("|")
-        if len(parts) < 7:
+        if len(parts) < 8:
             continue
 
         country = parts[1].upper()
-        rectype = parts[2].upper()
-        if (country not in countries) or (rectype not in rectypes):
+        record_type = parts[2].upper()
+        if (country not in countries) or (record_type not in record_types):
             continue
 
         start = parts[3]
         value = int(parts[4])
         date = parts[5]
         status = parts[6]
-        if rectype == "IPV4":
+        if record_type == "IPV4":
             # apnic|AU|ipv4|1.0.0.0|256|20110811|assigned|A91872ED
             print("%s/%d" % (start, length_to_cidr[value]))
-        elif rectype == "IPV6":
+        elif record_type == "IPV6":
             # apnic|CN|ipv6|2001:250::|35|20000426|allocated|A9235F14
             print("%s/%d" % (start, value))
 
